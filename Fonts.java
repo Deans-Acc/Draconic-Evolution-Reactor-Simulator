92244352b4e8e77827a36222c7206d5804e4873b
Dean <53697063+Deans-Acc@users.noreply.github.com>
2019-08-05 18:39:06 +0200

    Bugfix Nr. 2
    Fixed font not working.
    
    Now uses System Default Font

--- Fonts.java
+++ Fonts.java
@@ -1,15 +1,14 @@
 package app;
 import java.awt.*;
-
-
+import javax.swing.*;
 
 public class Fonts {
-    private static GraphicsEnvironment t = GraphicsEnvironment.getLocalGraphicsEnvironment();
-    private static Font[] fonts = t.getAllFonts();
-    private static Font tempfont;
-    private static Font font_big;
-    private static Font font_middle;
-    private static Font font_small;
+  //  private static GraphicsEnvironment t = GraphicsEnvironment.getLocalGraphicsEnvironment();
+   // private static Font[] fonts = t.getAllFonts();
+    private static Font tempfont = new JLabel().getFont();
+    private static Font font_big = tempfont.deriveFont(13F);
+    private static Font font_middle = tempfont.deriveFont(12F);
+    private static Font font_small = tempfont.deriveFont(10.25F);
     public static Font get_bigFont() {
         return font_big;
     }
@@ -19,16 +18,6 @@ public class Fonts {
     public static Font get_smallFont() {
         return font_small;
     }
-    public static void find_font() {
-        for (Font temp : fonts) {
-            System.out.println(temp.getName());
-            if(temp.getPSName() ==  "Segoe UI") {
-            
-            tempfont = temp;
-            }
-        }
-        font_big = tempfont.deriveFont(13F);
-        font_middle = tempfont.deriveFont(12F);
-        font_small = tempfont.deriveFont(10.25F);
-    }
+    
+
 }
